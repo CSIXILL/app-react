e058b275448c83edcbfaaac513067a1f50dfae2a
Your Name <you@example.com>
2023-01-25 17:19:33 +0200

    react-koulutus osa 1

---
+++ react-app/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Message } from "./pages/Message";
+import { Buttons } from "./pages/Buttons";
+import { Router } from "./pages/Router";
+import { Counters } from "./pages/Counters";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path='/' element={<Router />}>
+            <Route index element={<Message />} />
+            <Route path="Buttons" element={<Buttons />} />
+            <Route path="Counters" element={<Counters />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-app/src/pages/Router.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+import React from "react";
+
+export const Router = () => {
+    return (
+        <>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home </Link>
+                </li>
+                <li>
+                    <Link to="/buttons">Buttons</Link>
+                </li>
+                <li>
+                    <Link to="/counters">Counters</Link>
+                </li>
+            </ul>
+        </div>
+        <Outlet />
+        </>
+    );
+}
